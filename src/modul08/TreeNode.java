45d953a44c5994eb936c74dc871cc03632d6bff1
Irfan Triadi Saputra <72369571+Irfantriadis@users.noreply.github.com>
2021-06-12 13:44:54 +0700

    Add files via upload

---
+++ src/modul08/TreeNode.java
@@ -0,0 +1,10 @@
+package modul08;
+public class TreeNode {
+    char data;
+    TreeNode left;
+    TreeNode right;
+    
+    public TreeNode (int data){
+        this.data = (char) data;
+    }
+}
